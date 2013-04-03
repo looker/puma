6a6f6ec7d146be9a1c88a15fdfc23f4d4f6f2e60
bporterfield <benporterfield@gmail.com>
2013-04-03 13:09:54 -0700

    got bit by this. need to make sure something is loaded before monkey-patching! (in this case, openssl)

--- lib/open_ssl/ssl/ssl_server.rb
+++ lib/open_ssl/ssl/ssl_server.rb
@@ -1,3 +1,6 @@
+# make sure this is loaded before we monkey-patch
+require 'openssl'
+
 # we modify accept to only accept the socket, not do the ssl handshake. Puma does the handshake in a worker thread, so that 
 # clients cant block on the handshake
 module OpenSSL
