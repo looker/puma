2714add145f46c0123342ec2a81fd70cea21b9d0
Adam Markowitz <adam@looker.com>
2017-09-01 12:31:47 -0700

    resolve waiting counter problem
    if a thread is in the wait state, but transitions into
    a killed state the waiting counter will not be decremented
    leaving the pool to believe there is a thread available
    and waiting to complete work when that is not the case.
    Introduce a begin..ensure block so that any thread exception
    raised while in the wait state properly balances the
    waiting counter.

--- test/test_thread_pool.rb
+++ test/test_thread_pool.rb
@@ -240,4 +240,24 @@ class TestThreadPool < Test::Unit::TestCase
 
     assert_equal 0, pool.backlog
   end
+
+  def test_correct_waiting_count_for_killed_threads
+    pool = new_pool(1, 1) { |_| }
+
+    pause
+
+    # simulate our waiting worker thread getting killed for whatever reason
+    pool.instance_eval { @workers[0].kill }
+
+    pause
+
+    pool.reap
+
+    pause
+
+    pool << 0
+
+    pause
+    assert_equal 0, pool.backlog
+  end
 end
